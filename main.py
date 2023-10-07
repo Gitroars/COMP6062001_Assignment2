b8c03169017800e782e84f755249da01cb25d015
Christopher Owen <91533491+Own20@users.noreply.github.com>
2023-10-07 22:38:25 +0700

    second attempt

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
+"""
+A Scanner (Lexer) for the Calculator is the coding of the DFA in your favourite programming language. It recognize the valid strings (TOKEN) of Calculator and categorize the valid string into "tokens", which are the smallest meaningful lexical units of the Calculator language.
+INPUT : Calculator source code
+OUTPUT : TOKEN and the value of token (LEXEME) tokens
+
+Examples of INPUT:
+Celcius := 100.00
+Fahrenheit := (9/5)*Celcius + 32
+
+Example of OUTPUT:
+id: Celcius
+assign: :=
+number: 100.0
+id: Fahrenheit
+assign: :=
+lparen: (
+number: 9
+div: /
+number: 5
+rparen: )
+times: *
+id: Celcius
+plus: +
+number: 32
+"""
+
+# assign: :=
+# plus: +
+# minus: -
+# times: *
+# div: /
+# lparen: (
+# rparen: )
+# id: letter ( letter | digit )*
+# number: digit digit * | digit * ( . digit | digit . ) digit *
+# comment: /* ( non-* | * non-/ )* */ | //(non-newline)* newline
+
 import re
 
 # Define regular expressions for each lexical unit
@@ -10,8 +47,9 @@ regex_patterns = {
     'lparen': r'\(',
     'rparen': r'\)',
     'id': r'[a-zA-Z][a-zA-Z0-9]*',
-    'number': r'\d+(\.\d+)?',
-    'comment': r'/\*(.|\n)*?\*/|//.*'
+    'number': r'\d+\.\d+|\d*\.\d+|\d+\.\d*|\d+',
+    # 'comment': r'\/\*([^*]|(\*+[^*\/]))*\*+\/|\/\/[^\n]*',
+    'comment': r'\/\*[\s\S]*?\*\/|\/\/[^\n]*',
 }
 
 # Combine the regular expressions into a single pattern
@@ -25,8 +63,19 @@ def tokenize(expression):
         token_value = match.group(token_type)
         if token_type != 'COMMENT':
             tokens.append((token_type, token_value))
-    return tokens
+    for token_type, token_value in tokens:
+        print('\n'.join([f'{token_type}: {token_value}']))
 
+    # Print comments separately
+    comments = re.findall(regex_patterns['comment'], expression)
+    for comment in comments:
+        print(f'comment: {comment}')
 
-user_input = input()
-print(tokenize(user_input))
+# Test the scanner
+user_input = ""
+while True:
+    line = input("Input text (press Enter ONLY to finish): ")
+    if line == "":
+        break
+    user_input += line + "\n"
+tokenize(user_input)
